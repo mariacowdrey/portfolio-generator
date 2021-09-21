4503fd5848b2bc2f1bb69ffd5a41adb945489249
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-09-20 17:59:36 -0700

    create html

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i=0; i < profileDataArr.length; i +=1) {
-        console.log(profileDataArr[i]);
-    }
-    
-    console.log('================');
+const generatePage = require('./src/page-template.js');
 
-    // Is the same as this
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 
-};
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
